3442b66b15469477c8ea62e56c69ce3ef0927309
John. Sabbá <victorsabba@gmail.com>
2026-06-16 19:26:57 -0300

    Update

--- data/queries.sql
+++ data/queries.sql
@@ -1,7 +1,7 @@
 -- SQLite
--- DELETE FROM expenses WHERE 1=1;
--- DELETE FROM goals WHERE 1=1;
--- DELETE FROM incomes WHERE 1=1;
--- DELETE FROM user_settings WHERE 1=1;
-INSERT INTO users (name, email, pin) VALUES ('Demo User 2', 'demo2@klaro.app', '1234');
-INSERT INTO users (name, email, pin) VALUES ('Demo User 3', 'demo3@klaro.app', '1234');
+DELETE FROM expenses WHERE 1=1;
+DELETE FROM goals WHERE 1=1;
+DELETE FROM incomes WHERE 1=1;
+DELETE FROM user_settings WHERE 1=1;
+-- INSERT INTO users (name, email, pin) VALUES ('Demo User 2', 'demo2@klaro.app', '1234');
+-- INSERT INTO users (name, email, pin) VALUES ('Demo User 3', 'demo3@klaro.app', '1234');
